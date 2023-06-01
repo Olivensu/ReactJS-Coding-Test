9eaddaa010f8e9c3567cd6d397b4b93727a3d0b9
OliveNsu <tasbiul.hasan@northsouth.edu>
2023-06-01 16:52:47 +0600

    problem 1 done

--- src/components/Problem-1.jsx
+++ src/components/Problem-1.jsx
@@ -1,9 +1,56 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 const Problem1 = () => {
+    const [data, setData] = useState([]);
+  const [inputValue1, setInputValue1] = useState('');
+  const [inputValue2, setInputValue2] = useState('');
+  useEffect(() => {
+    const storedData = localStorage.getItem('data');
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    }
+  }, []);
+  const sortedData = [...data].sort((a, b) => {
+    if (a.value2 === 'Active' && b.value2 !== 'Active') {
+      return -1; 
+    } else if (a.value2 !== 'Active' && b.value2 === 'Active') {
+      return 1; 
+    } else {
+      return 0;
+    }
+  });
+  const finalSortedData = [...data].sort((a, b) => {
+    if (a.value2 === 'Active' && b.value2 !== 'Active') {
+      return -1; 
+    } else if (a.value2 !== 'Active' && b.value2 === 'Active') {
+      return 1; 
+    } else {
+      return 0;
+    }
+  });
+
+  const activeArray = data.filter((item) => item.value2 === 'Active');
+  const completeArray = data.filter((item) => item.value2 === 'Completed');
+  console.log(activeArray);
+  console.log(data);
 
     const [show, setShow] = useState('all');
 
+    const handleInputChange1 = (event) => {
+        setInputValue1(event.target.value);
+      };
+    
+      const handleInputChange2 = (event) => {
+        setInputValue2(event.target.value);
+      };
+    
+      const handleSave = () => {
+        const updatedData = [...data, { value1: inputValue1, value2: inputValue2 }];
+        setData(updatedData);
+        localStorage.setItem('data', JSON.stringify(updatedData));
+        setInputValue1('');
+        setInputValue2('');
+      };
     const handleClick = (val) =>{
         setShow(val);
     }
@@ -14,15 +61,16 @@ const Problem1 = () => {
             <div className="row justify-content-center mt-5">
                 <h4 className='text-center text-uppercase mb-5'>Problem-1</h4>
                 <div className="col-6 ">
-                    <form className="row gy-2 gx-3 align-items-center mb-4">
+                    <form  className="row gy-2 gx-3 align-items-center mb-4">
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Name"/>
+                            <input type="text" value={inputValue1} onChange={handleInputChange1} name='name' className="form-control" placeholder="Name"/>
                         </div>
                         <div className="col-auto">
-                            <input type="text" className="form-control" placeholder="Status"/>
+                            <input type="text" value={inputValue2} onChange={handleInputChange2} name='status' className="form-control" placeholder="Status"/>
                         </div>
                         <div className="col-auto">
-                            <button type="submit" className="btn btn-primary">Submit</button>
+                            <button type="submit" onClick={handleSave}  className="btn btn-primary">Submit</button>
+                            
                         </div>
                     </form>
                 </div>
@@ -47,7 +95,23 @@ const Problem1 = () => {
                         </tr>
                         </thead>
                         <tbody>
-                        
+                        {show==='all' ? sortedData.map((item, index) => (
+          <tr key={index}>
+            <td>{item.value1}</td>
+            <td>{item.value2}</td>
+          </tr>
+        )) : show==='active'? activeArray.map((item, index) => (
+            <tr key={index}>
+              <td>{item.value1}</td>
+              <td>{item.value2}</td>
+            </tr>
+          ))
+        : completeArray.map((item, index) => (
+            <tr key={index}>
+              <td>{item.value1}</td>
+              <td>{item.value2}</td>
+            </tr>
+          ))}
                         </tbody>
                     </table>
                 </div>
--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+
+import React, { useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 
 const Problem2 = () => {
+    const [show, setShow] = useState(false);
 
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
+      <div className="container">
+        <div className="row justify-content-center mt-5">
+          <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+          <div className="d-flex justify-content-center gap-3">
+            <Button  variant="primary" onClick={handleShow}>
+            All Contacts
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
+            <button className="btn btn-lg btn-outline-warning" type="button">
+              US Contacts
+            </button>
+          </div>
         </div>
+      </div>
     );
 };
 
